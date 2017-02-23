aaf36be16224e0073f24968da91fb091732c839c
unknown <neilha03@NSS010906.nss.scot.nhs.uk>
2017-02-23 14:01:06 +0000

    adding build code

---
+++ dist/js/app.js
@@ -0,0 +1,109 @@
+/**
+ * @name modules.js: Uses data attributes to instantiate modular components. Based on GOV.UK module pattern.
+ * @version 0.1.0: Fri, 27 Jan 2016 12:15:44 GMT
+ * @author neilha
+ * @license MIT
+ */
+;(function (global) {
+  'use strict'
+
+  var $ = global.jQuery;
+  
+  var NHSNSS = global.NHSNSS || {};
+  NHSNSS.Modules = NHSNSS.Modules || {};
+
+  NHSNSS.modules = {
+    find: function (container) {
+      container = container || $('body')
+
+      var modules;
+      var moduleSelector = '[data-module]';
+
+      modules = container.find(moduleSelector);
+
+      // Container could be a module too
+      if (container.is(moduleSelector)) {
+        modules = modules.add(container);
+      }
+
+      return modules;
+    },
+
+    start: function (container) {
+      var modules = this.find(container);
+
+      for (var i = 0, l = modules.length; i < l; i++) {
+        var module;
+        var element = $(modules[i]);
+        var type = camelCaseAndCapitalise(element.data('module'));
+        var started = element.data('module-started');
+
+        if (typeof NHSNSS.Modules[type] === 'function' && !started) {
+          module = new NHSNSS.Modules[type]();
+          module.start(element);
+          element.data('module-started', true);
+        }
+      }
+
+      // eg selectable-table to SelectableTable
+      function camelCaseAndCapitalise (string) {
+        return capitaliseFirstLetter(camelCase(string));
+      }
+
+      // http://stackoverflow.com/questions/6660977/convert-hyphens-to-camel-case-camelcase
+      function camelCase (string) {
+        return string.replace(/-([a-z])/g, function (g) {
+          return g[1].toUpperCase();
+        });
+      }
+
+      // http://stackoverflow.com/questions/1026069/capitalize-the-first-letter-of-string-in-javascript
+      function capitaliseFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+    }
+  }
+
+  global.NHSNSS = NHSNSS;
+})(window)
+/**
+ * @name someModule.js: an example module which alerts that the module has been started.
+ * @version 0.1.0: Fri, 27 Jan 2016 12:15:44 GMT
+ * @author neilha
+ * @license MIT
+ */
+;(function(global) {
+  'use strict'
+  var $ = global.jQuery;
+  
+  var NHSNSS = global.NHSNSS || {}
+  NHSNSS.Modules = NHSNSS.Modules || {}
+  
+  NHSNSS.Modules.SomeModule = function() {
+    this.start = function(element) {
+      // module code, start with event listeners
+	  alert('someModule started');
+    }
+  }
+})(window)
+/**
+ * @name app.js:
+ * @version 0.1.0: Fri, 27 Jan 2016 12:15:44 GMT
+ * @author neilha
+ * @license MIT
+ */
+ 
+;(function (global) {
+	'use strict'
+	var $ = global.jQuery;
+	var NHSNSS = global.NHSNSS || {};
+		
+	$(document).ready(function(){
+		// start modules
+		NHSNSS.modules.start();
+	});  
+  
+})(window)
+
+
+
---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * @name app.js:
+ * @version 0.1.0: Fri, 27 Jan 2016 12:15:44 GMT
+ * @author neilha
+ * @license MIT
+ */
+ 
+;(function (global) {
+	'use strict'
+	var $ = global.jQuery;
+	var NHSNSS = global.NHSNSS || {};
+		
+	$(document).ready(function(){
+		// start modules
+		NHSNSS.modules.start();
+	});  
+  
+})(window)
+
+
+
